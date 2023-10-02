c5864a66491d8f2cab50ba0342f6752198a1e726
SlammyTee <ogunniranabdulsalam@gmail.com>
2023-10-02 07:21:22 -0700

    changes made

--- 0x00-python-hello_world/6-concat.py
+++ 0x00-python-hello_world/6-concat.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-str1 = "Holberton"
+str1 = "Holberton "
 str2 = "School"
 #LINE OF CODE TO PRINT THE CONCATENATED STRINGS
-print(f"Welcome to {str1} {str2}!")
+print(f"Welcome to {str1 +  str2}!")
